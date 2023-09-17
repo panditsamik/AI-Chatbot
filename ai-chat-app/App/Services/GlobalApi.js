7535f639883e7da75f6136d62614d421318df0d3
Samik Pandit <panditsamik25@gmail.com>
2023-09-18 02:18:44 +0530

    Update GlobalApi.js

--- ai-chat-app/App/Services/GlobalApi.js
+++ ai-chat-app/App/Services/GlobalApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://192.168.0.105:3000/api/bardapi?ques=";
+const BASE_URL = "http://{Ip_Address}:3000/api/bardapi?ques=";
 
 const getBardApi = (userMsg) => axios.get(BASE_URL + userMsg);
 
